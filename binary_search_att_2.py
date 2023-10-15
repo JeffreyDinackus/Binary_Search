2ab1dec6e7eefbc9266b8b9fcb85f126486704c8
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-15 16:48:07 -0400

    adding answer

--- binary_search_att_2.py
+++ binary_search_att_2.py
@@ -1,34 +1,25 @@
-def Search(arr, l, r, x):
- 
-    while l <= r:
- 
-        mid = l + (r - l) // 2
- 
-        # Check if x is present at mid
-        if arr[mid] == x:
-            return mid
- 
-        # If x is greater, ignore left half
-        elif arr[mid] < x:
-            l = mid + 1
- 
-        # If x is smaller, ignore right half
-        else:
-            r = mid - 1
- 
-    # If we reach here, then the element
-    # was not present
-    return -1
- 
+def search(arr, low, high, guess):
 
   
 
- 
+  while low <= high:
+    mid = low + (high - low) // 2
+    print(guess, low, mid,  high)
+
+    if mid == guess:
+      return mid
+
+    elif arr[mid] < guess:
+      print("x")
+      low = mid + 1
+    else:
+      high = mid - 1
+  return -1
 if __name__ == '__main__':
   arr = [2, 3, 4, 10, 40,50,60]
   guess = 4
 
 
 
-  print(Search(arr, 0, len(arr)-1, guess))
+  print(search(arr, 0, len(arr)-1, guess))
 
